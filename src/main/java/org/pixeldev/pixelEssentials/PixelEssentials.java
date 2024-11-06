751d6a3c6b05865aae1f3ec001a5b3925fd2739b
Jordan <35157604+PixelFen@users.noreply.github.com>
2024-11-06 18:15:53 -0500

    Added new whitelist stuff

--- src/main/java/org/pixeldev/pixelEssentials/PixelEssentials.java
+++ src/main/java/org/pixeldev/pixelEssentials/PixelEssentials.java
@@ -1,5 +1,7 @@
 package org.pixeldev.pixelEssentials;
 
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.pixeldev.pixelEssentials.betterwhitelist.commands.WhitelistCommand;
 import org.pixeldev.pixelEssentials.commands.MainCmds;
 import org.pixeldev.pixelEssentials.commands.serverAdmin.ReloadConfigs;
 import org.pixeldev.pixelEssentials.commands.serverAdmin.VersionCmd;
@@ -10,6 +12,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.pixeldev.pixelEssentials.commands.Motd;
 import org.pixeldev.pixelEssentials.events.MemberJoin;
 
+import java.io.File;
+
 import static org.pixeldev.pixelEssentials.utils.ConfigValidation.updateConfig;
 
 public final class PixelEssentials extends JavaPlugin {
@@ -19,22 +23,58 @@ public final class PixelEssentials extends JavaPlugin {
         return instance;
     }
 
+
+
+
     public int getConfigVersions() {
         return 1;
     }
 
+    public static File whitelistCFile;
+    private FileConfiguration whitelistConfig;
 
-    public void loadConfig(){
+    private void createWhitelistConfig() {
+        whitelistCFile = new File(getDataFolder(), "whitelist.yml");
+
+        if (!whitelistCFile.exists()) {
+            whitelistCFile.getParentFile().mkdirs();
+            saveResource("whitelist.yml", false);
+        }
+    }
+
+    public FileConfiguration getWhitelistConfig() {
+        return this.whitelistConfig;
+    }
+
+    // This method saves the whitelist config
+    public void saveWhitelistConfig() {
+        try {
+            getWhitelistConfig().save(whitelistCFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadConfigs(){
         this.getConfig().options().copyDefaults(true);
         this.saveConfig();;
+
+        createWhitelistConfig();
+        this.whitelistConfig = YamlConfiguration.loadConfiguration(whitelistCFile);
+
+
+
     }
+
+
+
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         instance = this;
 
-        loadConfig();
+        loadConfigs();
         FileConfiguration config = getConfig();
 
         this.getLogger().info("Checking to see if there are any updates to the config...");
@@ -47,6 +87,8 @@ public final class PixelEssentials extends JavaPlugin {
         this.getCommand("version").setExecutor((new VersionCmd()));
 
         this.getCommand("pes").setExecutor((new MainCmds()));
+
+        this.getCommand("whitelist").setExecutor((new WhitelistCommand()));
 
         Bukkit.getPluginManager().registerEvents(new MemberJoin(), this);
 
